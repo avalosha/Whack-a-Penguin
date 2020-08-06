3050a361628e7967315c5c4dd9d5831479f2bcf9
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-08-06 16:20:13 -0500

    Setup project.

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Álvaro Ávalos Hernández on 06/08/20.
+//  Copyright © 2020 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
